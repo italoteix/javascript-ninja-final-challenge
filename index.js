4b20f2c6d9307100024c29d3147134c6268029ee
Italo Teixeira <italoteix@Italos-MacBook-Pro.local>
2019-08-25 23:07:16 -0300

    Challenge 31

--- index.js
+++ index.js
@@ -24,22 +24,34 @@
         var $tdYear = document.createElement("td");
         var $tdPlate = document.createElement("td");
         var $tdColor = document.createElement("td");
+        var $tdRemove = document.createElement("td");
+        var $remove = document.createElement("button");
 
         $image.src = $("#image").get().value;
         $tdImage.appendChild($image);
+
+        $remove.addEventListener("click", this.handleRemove);
+        $tdRemove.appendChild($remove);
 
         $tdBrand.textContent = $("#brand").get().value;
         $tdYear.textContent = $("#year").get().value;
         $tdPlate.textContent = $("#plate").get().value;
         $tdColor.textContent = $("#color").get().value;
+        $remove.textContent = "X";
 
         $tr.appendChild($tdImage);
         $tr.appendChild($tdBrand);
         $tr.appendChild($tdYear);
         $tr.appendChild($tdPlate);
         $tr.appendChild($tdColor);
+        $tr.appendChild($tdRemove);
 
         return $fragment.appendChild($tr);
+      },
+      handleRemove: function handleRemove(e) {
+        e.preventDefault();
+        var $tableCar = $('[data-js="table-car"]').get();
+        $tableCar.removeChild(e.target.parentElement.parentElement);
       },
       companyInfo: function companyInfo() {
         var ajax = new XMLHttpRequest();
